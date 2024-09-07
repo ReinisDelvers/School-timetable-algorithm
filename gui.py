187dcdceb5f5e248adbe5fecaf60c0ba34157d2c
ReinisDelvers <141557986+ReinisDelvers@users.noreply.github.com>
2024-09-07 12:19:37 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+import customtkinter
  
-class MyGUI:
+class MainGUI:
 
     def __init__(self):
 
@@ -8,6 +9,16 @@ class MyGUI:
 
         self.window.geometry("1280x720")
         self.window.title("School timetable algorithm")
+
+        self.menubar = tk.Menu(self.window)
+
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Close", command=exit)        
+        self.filemenu.add_separator()
+
+        self.menubar.add_cascade(menu=self.filemenu, label="File")
+
+        self.window.config(menu=self.menubar)
 
         self.buttonframe = tk.Frame(self.window)
         self.buttonframe.columnconfigure(0, weight=1)
@@ -29,10 +40,10 @@ class MyGUI:
         self.btn4 = tk.Button(self.buttonframe, text="Teachers", font=("Arial", 18), command=self.teachers)
         self.btn4.grid(row=0, column=3, sticky=tk.W+tk.E)
 
-        self.btn5 = tk.Button(self.buttonframe, text="a", font=("Arial", 18))
+        self.btn5 = tk.Button(self.buttonframe, text="", font=("Arial", 18))
         self.btn5.grid(row=0, column=4, sticky=tk.W+tk.E)
 
-        self.btn6 = tk.Button(self.buttonframe, text="a", font=("Arial", 18))
+        self.btn6 = tk.Button(self.buttonframe, text="", font=("Arial", 18))
         self.btn6.grid(row=0, column=5, sticky=tk.W+tk.E)
 
         self.buttonframe.pack(fill="x")
@@ -40,15 +51,31 @@ class MyGUI:
         self.window.mainloop()
         
     def subjects(self):
-        print("sub")
-
+        creator = tk.Toplevel(self.window) #creates a new window
+        creator.title("Subjects") #window title
+        creator.resizable(False, False) #prevents adjusting Widht, Height
+        creator.geometry("720x480") #sets window size
+        creator.grab_set() #prevents interacting with previous window
+        
     def classes(self):
-        print("cla")
+        creator = tk.Toplevel(self.window) #creates a new window
+        creator.title("Classes") #window title
+        creator.resizable(False, False) #prevents adjusting Widht, Height
+        creator.geometry("720x480") #sets window size
+        creator.grab_set() #prevents interacting with previous window
 
     def classrooms(self):
-        print("cle")
+        creator = tk.Toplevel(self.window) #creates a new window
+        creator.title("Classrooms") #window title
+        creator.resizable(False, False) #prevents adjusting Widht, Height
+        creator.geometry("720x480") #sets window size
+        creator.grab_set() #prevents interacting with previous window
 
     def teachers(self):
-        print("tea")
+        creator = tk.Toplevel(self.window) #creates a new window
+        creator.title("Teachers") #window title
+        creator.resizable(False, False) #prevents adjusting Widht, Height
+        creator.geometry("720x480") #sets window size
+        creator.grab_set() #prevents interacting with previous window
 
-MyGUI()
+MainGUI()
