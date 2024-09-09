923777dac9db3746e663cef9cb5e2802140c2db0
ReinisDelvers <141557986+ReinisDelvers@users.noreply.github.com>
2024-09-09 20:21:58 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -52,143 +52,260 @@ class MainGUI:
     
 
     def subjects(self):
-        creator = tk.Toplevel(self.window)  # creates a new window
-        creator.title("Subjects")  # window title
-        creator.resizable(False, False)  # prevents adjusting Width, Height
-        creator.geometry("720x480")  # sets window size
-        creator.grab_set()  # prevents interacting with previous window
+        wind = tk.Toplevel(self.window)  # creates a new window
+        wind.title("Subjects")  # window title
+        wind.resizable(False, False)  # prevents adjusting Width, Height
+        wind.geometry("720x480")  # sets window size
+        wind.grab_set()  # prevents interacting with previous window
 
-        buttonframe = tk.Frame(creator)
+        buttonframe = tk.Frame(wind)
         
         buttonframe.columnconfigure(0, weight=1)
         buttonframe.columnconfigure(1, weight=1)
         buttonframe.columnconfigure(2, weight=1)
 
-        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=subjectsadd)
+        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.subjectsadd)
         btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18))
+        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18), command=self.subjectsedit)
         btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
         
-        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18))
+        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18), command=self.subjectsremove)
         btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
 
         buttonframe.pack(fill="x")
 
-        creator.mainloop()
+        wind.mainloop()
 
     def subjectsadd(self):
-        creator = tk.Toplevel(self.window)  # creates a new window
-        creator.title("Subjects")  # window title
-        creator.resizable(False, False)  # prevents adjusting Width, Height
-        creator.geometry("720x480")  # sets window size
-        creator.grab_set()  # prevents interacting with previous window
+        wind = tk.Toplevel(self.window)  # creates a new window
+        wind.title("Subjects")  # window title
+        wind.resizable(False, False)  # prevents adjusting Width, Height
+        wind.geometry("720x480")  # sets window size
+        wind.grab_set()  # prevents interacting with previous window
 
-        buttonframe = tk.Frame(creator)
+        buttonframe = tk.Frame(wind)
         
         buttonframe.columnconfigure(0, weight=1)
-        buttonframe.columnconfigure(1, weight=1)
-        buttonframe.columnconfigure(2, weight=1)
+        buttonframe.columnconfigure(1, weight=2)
 
-        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18))
+        btn1 = tk.Label(buttonframe, text="Subjects title", font=("Arial", 18))
         btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18))
+        btn2 = tk.Entry(buttonframe,  font=("Arial", 18))
         btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
         
-        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18))
-        btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+        btn3 = tk.Label(buttonframe, text="Subjects short", font=("Arial", 18))
+        btn3.grid(row=1, column=0, sticky=tk.W+tk.E)
 
+        btn4 = tk.Entry(buttonframe,  font=("Arial", 18))
+        btn4.grid(row=1, column=1, sticky=tk.W+tk.E)
+
+        btn5 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.subjectsave)
+        btn5.grid(row=2, column=0, sticky=tk.W+tk.E)
 
         buttonframe.pack(fill="x")
 
-        creator.mainloop()
+        wind.mainloop()
+
+    def subjectsave(self):
+        print("a")
+
+    def subjectsedit(self):
+        print("a.1")
+    
+    def subjectsremove(self):
+        print("a.2")
 
     def classes(self):
-        creator = tk.Toplevel(self.window) #creates a new window
-        creator.title("Classes") #window title
-        creator.resizable(False, False) #prevents adjusting Widht, Height
-        creator.geometry("720x480") #sets window size
-        creator.grab_set() #prevents interacting with previous window
+        wind = tk.Toplevel(self.window) #creates a new window
+        wind.title("Classes") #window title
+        wind.resizable(False, False) #prevents adjusting Widht, Height
+        wind.geometry("720x480") #sets window size
+        wind.grab_set() #prevents interacting with previous window
         
-        buttonframe = tk.Frame(creator)
+        buttonframe = tk.Frame(wind)
         
         buttonframe.columnconfigure(0, weight=1)
         buttonframe.columnconfigure(1, weight=1)
         buttonframe.columnconfigure(2, weight=1)
 
-        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18))
+        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.classesadd)
         btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18))
+        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18), command=self.classesedit)
         btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
         
-        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18))
+        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18), command=self.classesremove)
         btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
 
         buttonframe.pack(fill="x")
 
-        creator.mainloop()
+        wind.mainloop()
+
+    def classesadd(self):
+        wind = tk.Toplevel(self.window)  # creates a new window
+        wind.title("Subjects")  # window title
+        wind.resizable(False, False)  # prevents adjusting Width, Height
+        wind.geometry("720x480")  # sets window size
+        wind.grab_set()  # prevents interacting with previous window
+
+        buttonframe = tk.Frame(wind)
+        
+        buttonframe.columnconfigure(0, weight=1)
+        buttonframe.columnconfigure(1, weight=2)
+
+        btn1 = tk.Label(buttonframe, text="Clases title", font=("Arial", 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        btn2 = tk.Entry(buttonframe,  font=("Arial", 18))
+        btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+        btn3 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.classessave)
+        btn3.grid(row=2, column=0, sticky=tk.W+tk.E)
+
+        buttonframe.pack(fill="x")
+
+        wind.mainloop()
+
+    def classessave(self):
+        print("b")
+    
+    def classesedit(self):
+        print("b.1")
+    
+    def classesremove(self):
+        print("b.2")
 
     def classrooms(self):
-        creator = tk.Toplevel(self.window) #creates a new window
-        creator.title("Classrooms") #window title
-        creator.resizable(False, False) #prevents adjusting Widht, Height
-        creator.geometry("720x480") #sets window size
-        creator.grab_set() #prevents interacting with previous window
+        wind = tk.Toplevel(self.window) #creates a new window
+        wind.title("Classrooms") #window title
+        wind.resizable(False, False) #prevents adjusting Widht, Height
+        wind.geometry("720x480") #sets window size
+        wind.grab_set() #prevents interacting with previous window
         
-        buttonframe = tk.Frame(creator)
+        buttonframe = tk.Frame(wind)
         
         buttonframe.columnconfigure(0, weight=1)
         buttonframe.columnconfigure(1, weight=1)
         buttonframe.columnconfigure(2, weight=1)
 
-        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18))
+        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.classroomsadd)
         btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18))
+        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18), command=self.classroomsedit)
         btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
         
-        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18))
+        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18), command=self.classroomsremove)
         btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
 
         buttonframe.pack(fill="x")
 
-        creator.mainloop()
+        wind.mainloop()
 
+    def classroomsadd(self):
+        wind = tk.Toplevel(self.window)  # creates a new window
+        wind.title("Subjects")  # window title
+        wind.resizable(False, False)  # prevents adjusting Width, Height
+        wind.geometry("720x480")  # sets window size
+        wind.grab_set()  # prevents interacting with previous window
+
+        buttonframe = tk.Frame(wind)
+        
+        buttonframe.columnconfigure(0, weight=1)
+        buttonframe.columnconfigure(1, weight=2)
+
+        btn1 = tk.Label(buttonframe, text="Classrooms title", font=("Arial", 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        btn2 = tk.Entry(buttonframe,  font=("Arial", 18))
+        btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+        btn3 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.classroomssave)
+        btn3.grid(row=2, column=0, sticky=tk.W+tk.E)
+
+        buttonframe.pack(fill="x")
+
+        wind.mainloop()
+
+    def classroomssave(self):
+        print("c")
+    
+    def classroomsedit(self):
+        print("c.1")
+    
+    def classroomsremove(self):
+        print("c.2")
+  
     def teachers(self):
-        creator = tk.Toplevel(self.window) #creates a new window
-        creator.title("Teachers") #window title
-        creator.resizable(False, False) #prevents adjusting Widht, Height
-        creator.geometry("720x480") #sets window size
-        creator.grab_set() #prevents interacting with previous window
+        wind = tk.Toplevel(self.window) #creates a new window
+        wind.title("Teachers") #window title
+        wind.resizable(False, False) #prevents adjusting Widht, Height
+        wind.geometry("720x480") #sets window size
+        wind.grab_set() #prevents interacting with previous window
         
-        buttonframe = tk.Frame(creator)
+        buttonframe = tk.Frame(wind)
         
         buttonframe.columnconfigure(0, weight=1)
         buttonframe.columnconfigure(1, weight=1)
         buttonframe.columnconfigure(2, weight=1)
 
-        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18))
+        btn1 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.teachersadd)
         btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18))
+        btn2 = tk.Button(buttonframe, text="Edit", font=("Arial", 18), command=self.teachersedit)
         btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
         
-        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18))
+        btn3 = tk.Button(buttonframe, text="Remove", font=("Arial", 18), command=self.teachersremove)
         btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
 
         buttonframe.pack(fill="x")
 
-        creator.mainloop()
+        wind.mainloop()
+
+    def teachersadd(self):
+        wind = tk.Toplevel(self.window)  # creates a new window
+        wind.title("Subjects")  # window title
+        wind.resizable(False, False)  # prevents adjusting Width, Height
+        wind.geometry("720x480")  # sets window size
+        wind.grab_set()  # prevents interacting with previous window
+
+        buttonframe = tk.Frame(wind)
+        
+        buttonframe.columnconfigure(0, weight=1)
+        buttonframe.columnconfigure(1, weight=2)
+
+        btn1 = tk.Label(buttonframe, text="Teachers name", font=("Arial", 18))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+        btn2 = tk.Entry(buttonframe,  font=("Arial", 18))
+        btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+        btn3 = tk.Button(buttonframe, text="Add", font=("Arial", 18), command=self.teacherssave)
+        btn3.grid(row=2, column=0, sticky=tk.W+tk.E)
+
+        buttonframe.pack(fill="x")
+
+        wind.mainloop()
+
+    def teacherssave(self):
+        print("d")
+    
+    def teachersedit(self):
+        print("d.1")
+    
+    def teachersremove(self):
+        print("d.2")
+    
+    
 
 MainGUI()
